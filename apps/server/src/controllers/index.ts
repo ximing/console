2e0c180b2528a57f9b85e8f463cf29bc16d2bf75
ximing <ximing@meituan.com>
2024-03-12 11:16:06 +0800

    feat: US-012 - Remove all controllers except auth and user

--- apps/server/src/controllers/index.ts
+++ apps/server/src/controllers/index.ts
@@ -1,39 +1,4 @@
-/* eslint-disable import/order */
-import { AIV1Controller } from './v1/ai.controller.js';
-import { ASRV1Controller } from './v1/asr.controller.js';
-import { AttachmentBAController } from './v1/attachment.ba.controller.js';
-import { AttachmentV1Controller } from './v1/attachment.controller.js';
 import { AuthV1Controller } from './v1/auth.controller.js';
-import { CategoryV1Controller } from './v1/category.controller.js';
-import { DebugBAController } from './v1/debug.ba.controller.js';
-import { ExploreController } from './v1/explore.controller.js';
-import { InsightsController } from './v1/insights.controller.js';
-import { MemoBAController } from './v1/memo.ba.controller.js';
-import { MemoV1Controller } from './v1/memo.controller.js';
-import { OcrV1Controller } from './v1/ocr.controller.js';
-import { PushRuleV1Controller } from './v1/push-rule.controller.js';
-import { TagV1Controller } from './v1/tag.controller.js';
 import { UserV1Controller } from './v1/user.controller.js';
-import { SystemController } from './v1/system.controller.js';
-import { StaticController } from './static.controller.js';
 
-// Note: StaticController import should stay last to avoid catching API routes
-export const controllers = [
-  MemoBAController,
-  MemoV1Controller,
-  InsightsController,
-  AuthV1Controller,
-  UserV1Controller,
-  CategoryV1Controller,
-  DebugBAController,
-  TagV1Controller,
-  AttachmentBAController,
-  AttachmentV1Controller,
-  ASRV1Controller,
-  OcrV1Controller,
-  ExploreController,
-  AIV1Controller,
-  PushRuleV1Controller,
-  SystemController,
-  StaticController,
-];
+export const controllers = [AuthV1Controller, UserV1Controller];
